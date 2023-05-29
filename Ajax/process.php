7e4e40a8e6f18b74919a6db782bf6e8ec21b2228
Gevin Šestak <gevin.sestak@hotmail.com>
2023-05-30 02:09:31 +0300

    Ajax

---
+++ Ajax/process.php
@@ -0,0 +1,14 @@
+<?php 
+
+echo "Processing...";
+// CHECK for POST variable
+if(isset($_POST['name'])) {
+    echo "POST: Your name is" . $_POST['name'];
+}
+
+// CHECK for GET variable
+if(isset($_GET['name'])) {
+    echo "GET: Your name is" . $_GET['name'];
+}
+
+?>
